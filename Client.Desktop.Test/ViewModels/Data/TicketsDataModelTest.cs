ab8c61bf13ae4fed65a9ddb429000db4a6ce107d
Khary92 <83533096+Khary92@users.noreply.github.com>
2025-06-25 10:28:06 +0200

    Added unit test projects `Client.Desktop.Test` and `Core.Server.Test` with initial test cases for `TicketsDataModel` and `AiSettingsCommandsService`. Updated solution file to include the new test projects.

---
+++ Client.Desktop.Test/ViewModels/Data/TicketsDataModelTest.cs
@@ -0,0 +1,39 @@
+using Client.Desktop.Communication.Commands;
+using Client.Desktop.Communication.Requests;
+using Client.Desktop.DTO;
+using Client.Desktop.Models.Data;
+using CommunityToolkit.Mvvm.Messaging;
+using Moq;
+
+namespace Client.Desktop.Test.ViewModels.Data;
+
+[TestFixture]
+[TestOf(typeof(TicketsDataModel))]
+public class TicketsDataModelTest
+{
+    [SetUp]
+    public void SetUp()
+    {
+        _commandSender = new Mock<ICommandSender>();
+        _requestSender = new Mock<IRequestSender>();
+
+        _messenger = new StrongReferenceMessenger();
+        _ticketDto = new TicketDto(Guid.NewGuid(), InitialName, InitialBookingNumber, InitialDocumentation,
+            _initialSprintIds);
+
+        _instance = new TicketsDataModel(_commandSender.Object, _requestSender.Object, _messenger);
+        _instance.RegisterMessenger();
+    }
+    
+    private Mock<ICommandSender> _commandSender;
+    private Mock<IRequestSender> _requestSender;
+    private StrongReferenceMessenger _messenger;
+
+    private TicketDto _ticketDto;
+    private TicketsDataModel _instance;
+
+    private const string InitialBookingNumber = "initialBookingNumber";
+    private const string InitialDocumentation = "initialDocumentation";
+    private const string InitialName = "initialName";
+    private readonly List<Guid> _initialSprintIds = [Guid.NewGuid()];
+}
