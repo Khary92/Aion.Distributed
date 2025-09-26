d379180df639f0d9b44e295bd7c136914718368d
Khary92 <jannic@hiegert.eu>
2025-09-26 14:58:15 +0200

    Added unit tests for `Verifier`: covered timer behavior, event emission, and report validation. Introduced Moq dependency in the test project for mock creation.

---
+++ Service.Monitoring.Test/Verifiers/Common/VerifierTest.cs
@@ -0,0 +1,119 @@
+using System.Reflection;
+using Moq;
+using Service.Monitoring.Shared;
+using Service.Monitoring.Shared.Enums;
+using Service.Monitoring.Verifiers.Common;
+using Service.Monitoring.Verifiers.Common.Enums;
+using Service.Monitoring.Verifiers.Common.Factories;
+using Service.Monitoring.Verifiers.Common.Records;
+
+namespace Service.Monitoring.Test.Verifiers.Common;
+
+[TestFixture]
+[TestOf(typeof(Verifier))]
+public class VerifierTest
+{
+    private Mock<IReportFactory> _reportFactoryMock;
+    private Guid _traceId;
+    private SortingType _sortingType;
+    private UseCaseMeta _useCaseMeta;
+    private List<TraceData> _traceDataList;
+    private Report _mockReport;
+
+    private Verifier _instance;
+    
+    [SetUp]
+    public void SetUp()
+    {
+        _traceId = Guid.NewGuid();
+        _sortingType = SortingType.Overview;
+        _useCaseMeta = UseCaseMeta.CreateNote;
+        _traceDataList =
+        [
+            new TraceData(
+                SortingType.Note,
+                UseCaseMeta.CreateNote,
+                LoggingMeta.ActionRequested,
+                "TestClass",
+                Guid.NewGuid(),
+                "Test log",
+                DateTimeOffset.UtcNow)
+        ];
+
+        _mockReport = new Report(
+            DateTimeOffset.UtcNow,
+            _sortingType,
+            _useCaseMeta,
+            Result.Success,
+            150,
+            _traceDataList,
+            _traceId);
+
+        _reportFactoryMock = new Mock<IReportFactory>();
+        _reportFactoryMock
+            .Setup(factory => factory.Create(
+                _traceId,
+                _sortingType,
+                _useCaseMeta,
+                It.IsAny<List<TraceData>>()))
+            .Returns(_mockReport);
+
+        _instance = new Verifier(_traceId, _sortingType, _useCaseMeta, _reportFactoryMock.Object);
+    }
+
+    [Test]
+    public void Add_ShouldStartAndStopTimer()
+    {
+        var traceData = new TraceData(
+            SortingType.Ticket,
+            UseCaseMeta.UpdateTicket,
+            LoggingMeta.CommandReceived,
+            "AnotherClass",
+            Guid.NewGuid(),
+            "Another log",
+            DateTimeOffset.UtcNow);
+
+        Assert.DoesNotThrow(() => _instance.Add(traceData));
+    }
+    
+    [Test]
+    public void VerificationCompleted_ShouldContainCorrectReportValues()
+    {
+        var eventRaised = false;
+        _instance.VerificationCompleted += (sender, report) =>
+        {
+            eventRaised = true;
+
+            Assert.That(report, Is.Not.Null);
+            Assert.That(_traceId, Is.EqualTo(report.TraceId));
+            Assert.That(_sortingType, Is.EqualTo(report.SortingType));
+            Assert.That(_useCaseMeta, Is.EqualTo(report.UseCase));
+            Assert.That(_traceDataList, Is.EqualTo(report.Traces));
+        };
+
+        _instance.GetType()
+            .GetMethod("Elapsed", BindingFlags.NonPublic | BindingFlags.Instance)
+            ?.Invoke(_instance, [null, null]);
+
+        Assert.That(eventRaised, "VerificationCompleted event was not raised.");
+    }
+    
+    [Test]
+    public void Timer_IsReset_EveryTimeAddIsCalled()
+    {
+        var traceData = new TraceData(
+            SortingType.Tag,
+            UseCaseMeta.CreateTag,
+            LoggingMeta.CommandReceived,
+            "TestClass",
+            Guid.NewGuid(),
+            "Test log",
+            DateTimeOffset.UtcNow);
+
+        Assert.DoesNotThrow(() =>
+        {
+            _instance.Add(traceData);
+            _instance.Add(traceData);
+        });
+    }
+}
