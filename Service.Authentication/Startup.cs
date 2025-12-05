690750641609ee04237367a467fc6dc21661da9c
Khary92 <jannic@hiegert.eu>
2025-12-05 22:34:48 +0100

    Removed ephemeral encryption key from OpenIddict configuration in `Startup.cs`.

--- Service.Authentication/Startup.cs
+++ Service.Authentication/Startup.cs
@@ -73,7 +73,6 @@ public class Startup
                 options.AllowClientCredentialsFlow();
 
                 options.AddSigningKey(rsaKey);
-                options.AddEphemeralEncryptionKey();  
 
                 options.UseAspNetCore()
                     .EnableTokenEndpointPassthrough();
